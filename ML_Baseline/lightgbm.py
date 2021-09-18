63ac77269c3f530e627315f6ad7fb51947af2e75
VivianELEK <88603143+VivianELEK@users.noreply.github.com>
2021-09-18 11:58:37 +0800

    Update lightgbm.py

--- ML_Baseline/lightgbm.py
+++ ML_Baseline/lightgbm.py
@@ -27,7 +27,7 @@ def LGB_test(train_x,train_y,test_x,test_y):
   if __name__ == '__main__':
 
     Preprocessor = Preprocessor_ML('../IEEE-CIS_Fraud_Detection')
-    test_mode = ['Changed','raw','DropMajorNull']
+    test_mode = ['merge_raw','raw','R-GCN','DropMajorNull']
     color_need = ['limegreen','salmon','cyan']
     plt.figure(figsize=(6, 6))
 
